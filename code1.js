be41137b8d6e6e7c7755b0e44cd37ade9549839d
nadavbaron20 <114684480+nadavbaron20@users.noreply.github.com>
2023-03-01 17:57:45 +0200

    Add files via upload

---
+++ code1.js
@@ -0,0 +1,32 @@
+const postsFetch = () => {
+    return fetch('https://jsonplaceholder.typicode.com/posts');
+};
+
+const todosFetch = () => fetch('https://jsonplaceholder.typicode.com/todos');
+
+const usersFetch = () => fetch('https://jsonplaceholder.typicode.com/users');
+
+const main = async () => {
+    // לא עובד מחזיר מערך של Promise
+    // const responses = [
+    //     await postsFetch(),
+    //     await todosFetch(),
+    //     await usersFetch()
+    // ];
+    // const datas = responses.map(async r => {
+    //     const ret = await r.json();
+    //     return await ret;
+    // });
+    // console.log(datas);
+
+    let response = await postsFetch();
+    const json = await response.json();
+    response = await todosFetch();
+    const json2 = await response.json();
+    response = await usersFetch();
+    const json3 = await response.json();
+
+    console.log(json, json2, json3);
+}
+
+main();
